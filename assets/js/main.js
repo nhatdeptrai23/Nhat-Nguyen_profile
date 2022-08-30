c8e920d7fb91ab86011c4d56226ad0a39aa39aaa
nhatdeptrai23 <japnh24>
2022-08-30 14:33:05 +0700

    upload source code

---
+++ assets/js/main.js
@@ -0,0 +1,137 @@
+/* ============Menu Show Hidden=========*/
+const navMenu = document.getElementById("nav-menu"),
+      navToggle = document.getElementById("nav-toggle"),
+      navClose = document.getElementById("nav-close");
+
+/* ============ Menu SHow======== */
+//Validate if constant exists
+if(navToggle){
+    navToggle.addEventListener("click", function(){
+        navMenu.classList.add("show-menu");
+    });
+};
+
+/* ============ Menu SHow======== */
+//Validate if constant exists
+if(navClose){
+    navClose.addEventListener("click", function(){
+        navMenu.classList.remove("show-menu");
+    });
+};
+
+/* ============ Remove Menu Mobile======== */
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction(){
+    const navMenu = document.getElementById("nav-menu");
+    //click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove("show-menu");
+};
+navLink.forEach(n => n.addEventListener("click", linkAction));
+
+/* ============ ACCORDION SKILLS======== */
+
+const skillsContent = document.getElementsByClassName("skills__content"),
+      skillsHeader = document.querySelectorAll(".skills__header");
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className;
+
+    for(let i=0; i< skillsContent.length; i++){
+        skillsContent[i].className = "skills__content skills__close";
+    };
+    if(itemClass === "skills__content skills__close" ){
+        this.parentNode.className= "skills__content skills__open"
+    };
+};
+
+skillsHeader.forEach( function (eL){
+    eL.addEventListener("click", toggleSkills);
+});
+
+
+/* ============ PORTFOLIO SWIPPER ======== */
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop:true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+  });
+
+  /* ============ SCROLL SECTIONS ACTIVE LINKS ======== */
+  const sections = document.querySelectorAll('section[id]')
+
+    function scrollActive(){
+        const scrollY = window.pageYOffset
+
+        sections.forEach(current =>{
+            const sectionHeight = current.offsetHeight
+            const sectionTop = current.offsetTop - 50;
+            sectionId = current.getAttribute('id')
+
+            if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+                document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+            }else{
+                document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+            }
+        })
+    }
+    window.addEventListener('scroll', scrollActive)
+
+      /* ============ CHANGE BACKGROUNG HEADER ======== */
+      function scrollHeader(){
+        const nav = document.getElementById("header");
+
+        if(this.scrollY >= 80) nav.classList.add("scroll-header");else nav.classList.remove("scroll-header");
+      };
+
+      window.addEventListener("scroll", scrollHeader);
+
+            /* ============ Show Scroll Up ======== */
+
+    function scrollUp(){
+        const scrollUp = document.getElementById("scroll-up");
+        if(this.scrollY >=560) scrollUp.classList.add("show-scroll"); else scrollUp.classList.remove("show-scroll");
+    }
+
+    window.addEventListener("scroll", scrollUp);
+
+
+      /* ============ DARK LIGHT THEME ======== */
+
+    const themeButton = document.getElementById("theme-button");
+    const darkTheme = "dark-theme"
+    const iconTheme = "uil-sun"
+
+    // previously selected topic
+    const selectedTheme = localStorage.getItem("selected-theme");
+    const selectedIcon = localStorage.getItem("selected-icon");
+
+    const getCurrentTheme = function(){
+        document.body.classList.contains(darkTheme) ? "dark": "light";
+    }
+
+    const getCurrentIcon = function(){
+        themeButton.classList.contains(iconTheme) ? "uil-moon": "uil-sun";
+    }
+
+    if(selectedTheme){
+        document.body.classList[selectedTheme === "dark"? "add": "remove"](darkTheme);
+        themeButton.classList[selectedIcon === "uil-moon"? "add": "remove"](iconTheme);
+
+    };
+
+    themeButton.addEventListener("click", function(){
+        document.body.classList.toggle(darkTheme);
+        themeButton.classList.toggle(iconTheme);
+
+        localStorage.setItem("selected-theme", getCurrentTheme());
+        localStorage.setItem("selected-icon", getCurrentIcon());
+    })
